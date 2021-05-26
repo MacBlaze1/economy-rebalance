43fb3e11869aeaa95711990bb71ad756d0130766
Blase <fuckoff>
2021-05-25 23:58:56 -0500

    gah

--- src/main/java/economyrebalance/EconomyRebalancePlugin.java
+++ src/main/java/economyrebalance/EconomyRebalancePlugin.java
@@ -2,25 +2,43 @@ package economyrebalance;
 
 import com.fs.starfarer.api.Global;
 import com.fs.starfarer.api.BaseModPlugin;
+import com.fs.starfarer.api.campaign.comm.IntelInfoPlugin;
+import com.fs.starfarer.api.campaign.econ.MarketAPI;
 import com.fs.starfarer.api.campaign.BaseCampaignEventListener;
+import com.fs.starfarer.api.impl.campaign.intel.BaseIntelPlugin;
+import com.fs.starfarer.api.ui.TooltipMakerAPI;
 import org.apache.log4j.Logger;
-import economyrebalance.util.TariffUtil;
-
 public class EconomyRebalancePlugin extends BaseModPlugin
-{   
+{
     public static Logger log = Global.getLogger(EconomyRebalancePlugin.class);
-    
+
     @Override
     public void onGameLoad(boolean newGame) {
         new EconUtil();
+        new MissionUtil();
         log.info("EconomyRebalance: Loaded");
     }
 }
 
 public class EconUtil extends BaseCampaignEventListener {
     public static Logger log = Global.getLogger(EconUtil.class);
+
     public EconUtil() {
         super(false);
         Global.getSector().addTransientListener(this);
+    }
+
+    @Override
+    public class reportPlayerOpenedMarket(MarketAPI market) {
+        float rate = 10.0f;
+        market.getTariff().setBaseValue(rate);
+    }
+}
+
+public class MissionUtil extends BaseCampaignEventListener {
+    public MissionUtil()
+    {
+        super(false);
+
     }
 }
